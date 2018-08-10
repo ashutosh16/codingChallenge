6ccc2eb520cd8946af3ad11f5529f6bdd637a8b2
sameer girkar <sgirkar@apple.com>
2018-08-10 00:08:50 -0700

    Challenge 1 test data

--- challenge_1.js
+++ challenge_1.js
@@ -1,10 +1,12 @@
 // Write a function which take two arguments,
 // return the 2 elemens from array 'a',
 // whose sum is equal to second parameter of function.
+// @function find
 // @param {Array} 'a' array of numbers
 // @param {Number} 'sum' number 
+// @returns {Array} array of two number
 // Time complexity of this function is O(n)
-function sum(a, sum) {
+function find(a, sum) {
   let temp = {};
   for (let i = 0; i <= a.length; i++) {
     if (typeof temp[a[i]] !== 'undefined') {
@@ -21,3 +23,7 @@ function sum(a, sum) {
   }
   return 'not found';
 }
+let A = [1,4,7,2,15,67];
+let sum = 17;
+find(A, sum);
+// expected output: [15,2] or [2,15]
