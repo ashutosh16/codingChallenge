41d43f2be4e5e73f0e2702c16587f09eac3d1388
sameer girkar <sgirkar@apple.com>
2018-08-23 23:47:52 -0700

    no message

--- challenge_5.js
+++ challenge_5.js
@@ -1,34 +1,20 @@
-// function sort(arr) {
-//   let i = 1,
-//     j = arr.length - 1,
-//     newArr = [arr[0]];
+function sort(arr) {
+  let i = 1,
+    j = arr.length - 1,
+    newArr = [arr[0]];
 
-//   while (arr[i] > arr[i - 1]) {
-//     newArr.push(arr[i]);
-//     i++;
-//   }
-//   while (j >= i) {
-//     newArr.unshift(arr[j]);
-//     j--;
-//   }
-
-//   return newArr;
-// }
-
-// var sortedArr = sort([1, 2, 3, 4, 5]);
-// console.log(sortedArr);
-
-
-function reverse(str, index = 0) {
-  if((str.length/2) > index) {
-    console.log(index);
-    let temp = str[index];
-    str[index] = str[str.length - 1 - index];
-    str[str.length - index] = temp;
-    return reverse(str, index+1);
-  } else {
-    return str;
+  while (arr[i] > arr[i - 1]) {
+    newArr.push(arr[i]);
+    i++;
   }
+  while (j >= i) {
+    newArr.unshift(arr[j]);
+    j--;
+  }
+
+  return newArr;
 }
 
-console.log(reverse('sameer'));
+var sortedArr = sort([1, 2, 3, 4, 5]);
+console.log(sortedArr);
+
