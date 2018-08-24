cd9f00e497d5fab01dfb74fae5853ada68106e8c
sameer girkar <sgirkar@apple.com>
2018-08-23 23:37:54 -0700

    shortest path in multistage graph

--- challenge_5.js
+++ challenge_5.js
@@ -1,19 +1,34 @@
-function sort(arr) {
-  let i = 1,
-    j = arr.length - 1,
-    newArr = [arr[0]];
+// function sort(arr) {
+//   let i = 1,
+//     j = arr.length - 1,
+//     newArr = [arr[0]];
 
-  while (arr[i] > arr[i - 1]) {
-    newArr.push(arr[i]);
-    i++;
-  }
-  while (j >= i) {
-    newArr.unshift(arr[j]);
-    j--;
-  }
+//   while (arr[i] > arr[i - 1]) {
+//     newArr.push(arr[i]);
+//     i++;
+//   }
+//   while (j >= i) {
+//     newArr.unshift(arr[j]);
+//     j--;
+//   }
 
-  return newArr;
+//   return newArr;
+// }
+
+// var sortedArr = sort([1, 2, 3, 4, 5]);
+// console.log(sortedArr);
+
+
+function reverse(str, index = 0) {
+  if((str.length/2) > index) {
+    console.log(index);
+    let temp = str[index];
+    str[index] = str[str.length - 1 - index];
+    str[str.length - index] = temp;
+    return reverse(str, index+1);
+  } else {
+    return str;
+  }
 }
 
-var sortedArr = sort([1, 2, 3, 4, 5]);
-console.log(sortedArr);
+console.log(reverse('sameer'));
