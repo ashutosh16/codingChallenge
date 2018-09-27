542e162d8d60fc7e96a957e8b60a6765fa45bd44
Sameer Girkar <sam.girkar@gmail.com>
2018-09-27 02:23:10 -0700

    Find a triplet that sum to a given value with space complexity O(1)

--- FindTripletThatSumToGivenValue.js
+++ FindTripletThatSumToGivenValue.js
@@ -19,8 +19,16 @@ function findAllTriplet(a, number){
 findAllTriplet([12,3,4,1,6,9]);
 
 /**
- * Can be solved by O(n^2)
+ * Can be solved by O(n^2), Space complexity is O(n)
  * Step 1 - for i=0 to n
  * find the dupleats from a[i+1] to a[n] such that sum of duplates is (number - a[i]) == (a[d1]+a[d2])
- *  
+ * 
+ */
+
+/**
+ * Can be solved by O(n^2), Space complexity is O(1)
+ * Step 1 - Sort the array - O(nlog(n))
+ * Step 2 - set first element for i=0 to n - 3
+ * Step 3 - left = a[i+1] right = a[n-1] while (left < right) left++ & right--;
+ * 
  */
