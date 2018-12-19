ac04944615e4681a27f6705260e4729b918196d2
Sameer Girkar <sam.girkar@gmail.com>
2018-12-18 16:57:34 -0800

    Find maximum product of two integers in an array

--- Array/Array_MaxProductOfTwoNumber.js
+++ Array/Array_MaxProductOfTwoNumber.js
@@ -1,7 +1,6 @@
 // Find maximum product of two integers in an array
 
-// 1. firstMax & secondMax
-// 2. firstMin & secondMin
+// Maximum product = (firstMax * secondMax)  or (firstMin * secondMin)
 // Time complexity O(n)
 
 function findMaxProduct(arr) {
