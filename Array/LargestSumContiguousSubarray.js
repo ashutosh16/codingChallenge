778b49e2d2eda7c9603b4c9eaae758e5c0c28b67
Sameer Girkar <sam.girkar@gmail.com>
2019-10-15 01:16:32 -0700

    Update LargestSumContiguousSubarray.js

--- Array/LargestSumContiguousSubarray.js
+++ Array/LargestSumContiguousSubarray.js
@@ -8,3 +8,23 @@
 // add element in currentSum as long as currentSum is +ve
 // if currentSum > maxSum then maxSum = currentSum
 // if currentSum is -ve or 0 then make currentSum = 0
+
+function LargesSumContiguousSubarray(a) {
+  let maxSum = null, currentSum = 0;
+  
+  for(let i = 0; i < a.length; i++){
+    currentSum += a[i];
+    
+    if(currentSum > maxSum || maxSum === null) {
+      maxSum = currentSum;
+    }
+    
+    if(currentSum < 0 ){
+      currentSum = 0
+    }
+  }
+  return maxSum;
+}
+
+var LargestSum = LargesSumContiguousSubarray([-2, -3, 4, -1, -2, 1, 5, -3]);
+alert(" Largest sum "+ LargestSum); 
