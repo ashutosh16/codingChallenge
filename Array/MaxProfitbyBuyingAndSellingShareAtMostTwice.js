a33b42a92a7f66662ba642577de96dda2eff3dd8
Sameer Girkar <sam.girkar@gmail.com>
2019-10-15 01:21:04 -0700

    Update MaxProfitbyBuyingAndSellingShareAtMostTwice.js

--- Array/MaxProfitbyBuyingAndSellingShareAtMostTwice.js
+++ Array/MaxProfitbyBuyingAndSellingShareAtMostTwice.js
@@ -1,4 +1,9 @@
-// Maximum profit by buying and selling a share at most twice
+/** Maximum profit by buying and selling a share at most twice
+* In a daily share trading, a buyer buys shares in the morning and sells it on the same day. 
+* If the trader is allowed to make at most 2 transactions in a day, 
+* whereas the second transaction can only start after the first one is complete (Buy-> Sell-> Buy-> Sell).
+* Given stock prices throughout the day, find out the maximum profit that a share trader could have made.
+**/
 // Time Complexity O(n)
 {
   function maxProfit (price) {
