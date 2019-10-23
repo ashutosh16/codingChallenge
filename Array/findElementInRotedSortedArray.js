76bb273eb9b33b2b9a6c559ea3798697520fd0ee
Sameer Girkar <sam.girkar@gmail.com>
2019-10-23 01:38:35 -0700

    Update findElementInRotedSortedArray.js

--- Array/findElementInRotedSortedArray.js
+++ Array/findElementInRotedSortedArray.js
@@ -9,20 +9,25 @@ function search(a, key, l =0, h=a.length-1){
   } else if(l >= h) { 
     return "not Found";
   }
+  //We can only confirn that element is present or not in sorted part of the array by looking the low and hight of sorted part.
   
   // If Left array is sorted
   if(a[l] < a[mid]) {
+    //If key present in left array
     if(key < a[mid] && key >= a[l]){
       return search(a, key, l, mid-1);
     } else {
+      //Key must be present in right array.
       return search(a, key, mid+1, h);
     }
   }
   
   // As Left array is not sorted, Right array is sorted.
   if(key <= a[h] && key > a[mid]) {
+    //If key present in right array
     return search(a, key, mid+1, h);
   } else {
+    //Key must be present in left array
     return search(a, key, l, mid-1);
   }
 }
