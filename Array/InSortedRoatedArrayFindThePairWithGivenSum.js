87278804ddf92c0ce8773b5189f3ba1ff1f1c176
Sameer Girkar <sam.girkar@gmail.com>
2019-10-23 02:28:53 -0700

    Update InSortedRoatedArrayFindThePairWithGivenSum.js

--- Array/InSortedRoatedArrayFindThePairWithGivenSum.js
+++ Array/InSortedRoatedArrayFindThePairWithGivenSum.js
@@ -1,1 +1,41 @@
 
+//Given a sorted and rotated array, find if there is a pair with a given sum
+// Input: arr[] = {11, 15, 6, 8, 9, 10}, x = 16
+// Output: true
+// There is a pair (6, 10) with sum 16
+
+// Input: arr[] = {11, 15, 26, 38, 9, 10}, x = 35
+// Output: true
+// There is a pair (26, 9) with sum 35
+
+// Input: arr[] = {11, 15, 26, 38, 9, 10}, x = 45
+// Output: false
+// There is no pair with sum 45.
+
+
+function find(a, sum) {
+  let n = a.length;
+  //find the pivot point in the array.
+  for(var i= 0; i < a.length; i++){
+    if(a[i] > a[i+1]){
+      break;
+    }
+  }
+  
+  let h = i,
+      l = (n+i+1) % n;
+  
+  while(l !== h) {
+    var currentSum = a[l]+a[h];
+    if(currentSum === sum){
+      return [l,h];
+    }
+    if(currentSum < sum) {
+      l = (n+l+1) % n;
+    } else
+    if(currentSum > sum) {
+      h = (h+1+n) % n;
+    }
+  }
+  return "Not Found";
+}
