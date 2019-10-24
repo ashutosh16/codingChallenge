2c01224324d43e04e7b9af3409b900a30685f104
Sameer Girkar <sam.girkar@gmail.com>
2019-10-24 00:15:36 -0700

    Update findTheNumberOfRotations.js

--- Array/findTheNumberOfRotations.js
+++ Array/findTheNumberOfRotations.js
@@ -1,3 +1,21 @@
+
+// Find the Rotation Count in Rotated Sorted array
+// Consider an array of distinct numbers sorted in increasing order.
+// The array has been rotated (clockwise) k number of times.
+// Given such an array, find the value of k.
+
+// Input : arr[] = {15, 18, 2, 3, 6, 12}
+// Output: 2
+// Explanation : Initial array must be {2, 3,
+// 6, 12, 15, 18}. We get the given array after 
+// rotating the initial array twice.
+
+// Input : arr[] = {7, 9, 11, 12, 5}
+// Output: 4
+
+// Input: arr[] = {7, 9, 11, 12, 15};
+// Output: 0
+
 function findRotation(a, l=0, h=a.length-1) {
   if(a[l] < a[h]) {
     return l;
@@ -5,7 +23,7 @@ function findRotation(a, l=0, h=a.length-1) {
   
   var mid = Math.floor((h-l) / 2);
   if(a[mid] < a[mid-1]) {
-    returnm mid;
+    return mid;
   }
   if(a[mid] > a[mid+1]) {
     return mid+1;
@@ -16,8 +34,8 @@ function findRotation(a, l=0, h=a.length-1) {
   } else {
     return findRotation(a, l, mid-1);
   }
-  
-  
-  
-  
 }
+
+findRotation([15, 18, 2, 3, 6, 12]);
+findRotation([7, 9, 11, 12, 5]);
+findRotation([7, 9, 11, 12, 15]);
