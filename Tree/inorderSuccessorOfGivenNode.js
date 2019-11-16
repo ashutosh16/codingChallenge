9f7a8adb47e101c2819dd804d1287ac06b8a308f
Sameer Girkar <sam.girkar@gmail.com>
2019-11-16 02:01:59 -0800

    Update inorderSuccessorOfGivenNode.js

--- Tree/inorderSuccessorOfGivenNode.js
+++ Tree/inorderSuccessorOfGivenNode.js
@@ -1,2 +1,39 @@
-// Inorder Successor of a node in Binary Tree
-// https://www.geeksforgeeks.org/inorder-succesor-node-binary-tree/
+{
+  function Node(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+
+  function findInorderSuccessor(root, data, p = { next: null }) {
+    if (root) {
+      let result = findInorderSuccessor(root.right, data, p);
+      if (result) {
+        if (root.data === data) {
+          return p.next;
+        }
+        p.next = root.data;
+        return findInorderSuccessor(root.left, data, p);
+      }
+    } else {
+      return p.next;
+    }
+  }
+
+  let root = new Node(1);
+  root.left = new Node(2);
+  root.right = new Node(3);
+  root.left.left = new Node(4);
+  root.left.right = new Node(5);
+  root.right.left = new Node(6);
+  root.right.right = new Node(7);
+
+  printInorderSuccessorForAllNodes(root);
+}
+
+//Input
+/*         1        */
+/*       /   \      */
+/*     2      3     */
+/*    /  \  /   \   */
+/*   4   5  6   7   */
