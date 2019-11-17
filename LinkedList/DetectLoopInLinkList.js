9df5457a6d76e21026e2e19abcf7f9de3a712a21
Sameer Girkar <sam.girkar@gmail.com>
2019-11-17 02:23:44 -0800

    Update DetectLoopInLinkList.js

--- LinkedList/DetectLoopInLinkList.js
+++ LinkedList/DetectLoopInLinkList.js
@@ -4,18 +4,34 @@
 //    1. Find size of the loop
 //    2. Find starting point of the loop
 //    3. Loop from loopStartingPoint and make pre of loopStartingPoint as null
+{
+  function Node(data) {
+    this.next = null;
+    this.data = data;
+  }
+  
+  function IsLoopInLinkList(head) {
+    if(!head) return false;
+    let slow = head,
+        fast = head.next;
 
-slow = head;
-fast = slow.next;
-while(fast !== null || slow !== fast){
-  slow = slow.next
-  fast = fast.next.next;
-}
-if(fast === null) {
-  return false
-} else {
-  // Loop detected. 
-  // Find size of the loop
-  // Fast & slow may not be starting at the loop
-
+    while( fast !== null && slow !== fast) {
+      slow = slow.next;
+      fast = (fast.next) ? fast.next.next : null;
+    }
+    
+   return (slow && fast && slow === fast) ? true : false;
+  }
+  
+  let head = new Node(1);
+  head.next = new Node(2);
+  head.next.next = new Node(3);
+  head.next.next.next = new Node(4);
+  head.next.next.next.next = new Node(5);
+  
+  head.next.next.next.next.next = head.next;
+  let p = head;
+  
+  console.log(IsLoopInLinkList(head));
+  
 }
