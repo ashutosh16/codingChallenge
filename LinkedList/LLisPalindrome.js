927d6f4ac1d5995ee4fe6bd7f5ef6f32a51de160
Sameer Girkar <sam.girkar@gmail.com>
2019-11-17 03:21:34 -0800

    Update LLisPalindrome.js

--- LinkedList/LLisPalindrome.js
+++ LinkedList/LLisPalindrome.js
@@ -1,7 +1,41 @@
 /**
  * Check is singly link list is pelindrome
  * Time complexity O(n)
+ * The idea is to use function call stack as container. Recursively traverse till the end of list. 
+ * When we return from last NULL, we will be at last node.
+ * The last node to be compared with first node of list.
+ * In order to access first node of list, we need list head to be available in the last call of recursion.
+ * Hence we pass head also to the recursive function. If they both match we need to compare (2, n-2) nodes.
+ * Again when recursion falls back to (n-2)nd node, we need reference to 2nd node from head. 
+ * We advance the head pointer in previous call, to refer to next node in the list.
  */
+
+
+
+{
+
+  function Node(data){
+    this.next = null;
+    this.data = data;
+  }
+  
+  function isPalindrome(left, right = left) {
+    if(!right) return true;
+    let result = isPalindrome(left, right.next);
+    if(result){
+      if(left.data === right.data) {
+        result = true;
+        left = left.next;
+      } else {
+        result = false;
+      }
+    }
+    return result;  
+  }
+}
+//----------------------------------------------
+
+
 {
   class Node {
     constructor(data) {
