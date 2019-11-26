ed7ad0bfc9a1e8fc2ac46482b7ed1d86b7e7934f
Sameer Girkar <sam.girkar@gmail.com>
2019-11-25 23:39:04 -0800

    Create NRetries.js

---
+++ javascript/NRetries.js
@@ -0,0 +1,43 @@
+{
+    function TryToSuccess(fun, reties) {
+        let attempt = 0,
+            isSuccess = false;
+
+        let doTry = (...args) => {
+            attempt++;
+            return fun(...args)
+                    .catch((err) => {
+                        console.log("fail ", attempt);
+                        if(attempt <= reties){
+                            return doTry(...args);
+                        } else {
+                            return Promise.reject(err);
+                        }
+                    });
+        }
+
+        return doTry;
+    }
+
+    function asyncFunction(){
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                (window.findResult === true) ? resolve("Done") : reject("fail");
+            }, 2000);
+        });
+    }
+
+    var cloneFunc = TryToSuccess(asyncFunction, 3);
+
+    cloneFunc()
+        .then(res => { 
+            console.log("Got Success. ", res)
+        })
+        .catch(err => { 
+            console.log("Rejected with err ", err); 
+        });
+
+    setTimeout(() => {
+        window.findResult = true;
+    }, 4000);
+}
