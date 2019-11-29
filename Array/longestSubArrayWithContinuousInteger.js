b6c212e89f07d118d2a67f26577451a0c71b8e7d
Sameer Girkar <sam.girkar@gmail.com>
2019-11-28 16:35:03 -0800

    Update longestSubArrayWithContinuousInteger.js

--- Array/longestSubArrayWithContinuousInteger.js
+++ Array/longestSubArrayWithContinuousInteger.js
@@ -28,7 +28,7 @@ let a = [1, 56, 58, 57, 90, 92, 94, 93, 91, 45];
 let result = longSubArray(a);
 console.log(result);
 
-
+// Time Complexity is O(n*n), Space Complexity O(1)
 // Hint : 
 // 1) Start finding sub array starting from index 0 to n-1, Then index 1 to n-1 and so on.
 // 2) If in subarray keep on updating minElement and maxElement from the sub array.
@@ -36,3 +36,12 @@ console.log(result);
 //    ** Then sub array has the element that can be form a sequence.
 // e.g. [8, 4, 6, 5, 3] 
 // Min = 3, Max = 8, lenght = 5 Hence Length = (Max - Min) Then its required sub array.
+
+// --------------------------------------------------------------------------------------------------- //
+
+// -----------------------------   Solution 2  ------------------------------------------------------ //
+// Hint:
+// 1) Sort the given array Time complexity O(n * log(n))
+// 2) find sub array with a[i+1] === a[i] + 1;
+
+
