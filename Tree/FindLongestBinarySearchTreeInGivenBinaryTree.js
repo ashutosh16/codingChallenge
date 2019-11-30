91d2ee6f181adfebb173b144088403535e409eeb
Sameer Girkar <sam.girkar@gmail.com>
2019-11-29 18:36:45 -0800

    Update FindLongestBinarySearchTreeInGivenBinaryTree.js

--- Tree/FindLongestBinarySearchTreeInGivenBinaryTree.js
+++ Tree/FindLongestBinarySearchTreeInGivenBinaryTree.js
@@ -63,19 +63,27 @@ bt2.right.right.right = new BST(80);
 bt2.right.right.left = new BST(65);
 let longestBST = null;
 
+//HINT: https://www.youtube.com/watch?v=4fiDs7CCxkc
+
 bt2.travel((node) => {
-  let leftLength = node.left ? node.left.length : 0; //Length of all left node
-  let rightLength = node.right ? node.right.length : 0; //Length of all right node
-  node.length = leftLength + rightLength + node.length; //Length of a tree
+  let leftLength = node.left ? node.left.length : 0; //Length of left sub tree
+  let rightLength = node.right ? node.right.length : 0; //Length of right sub tree
+  node.length = leftLength + rightLength + node.length; //Length of a tree = 1 + leftSubTree + rightSubTree
 
-  node.minVal = (node.left && node.left.minVal < node.val) ? node.left.minVal : node.val; //Min val in tree
-  node.minVal = (node.right && node.right.minVal < node.minVal) ? node.right.minVal : node.minVal;
+  // node.minVal is Min val from tree with root as current node.
+  node.minVal = (node.left && node.left.minVal < node.val) ? node.left.minVal : node.val; //comapare with left subTree MinVal
+  node.minVal = (node.right && node.right.minVal < node.minVal) ? node.right.minVal : node.minVal; //comapare with right subTree MinVal
 
-  node.maxVal = (node.left && node.left.maxVal > node.val) ? node.left.maxVal : node.val;//Max value in tree
+  // node.maxVal is Max val from tree with root as current node.
+  node.maxVal = (node.left && node.left.maxVal > node.val) ? node.left.maxVal : node.val;
   node.maxVal = (node.right && node.right.maxVal > node.maxVal) ? node.right.maxVal : node.maxVal;
 
-  //maxValue in left tree < current node value < minValue from right sub tree --> Then is BST
-  node.isBST = ((node.left) ? (node.left.maxVal < node.val) : true) && ((node.right) ? (node.right.minVal > node.val) : true);
+  // Condition to current tree is BST
+  // 1) left subTree is BST && right subTree is BST
+  // 2) node.left.max <= node.data < node.rihgt.min
+  node.isBST = ((node.left) ? (node.left.isBST && (node.left.maxVal < node.val)) : true) && 
+               ((node.right) ? node.right.isBST && (node.right.minVal > node.val)) : true);
+  
   if (node.isBST){
     if (longestBST) {
       if (longestBST.length < node.length) {
