96855b3e7cfaa0d95238ac1f8d93acee24cc2eb1
Sameer Girkar <sam.girkar@gmail.com>
2019-12-01 17:34:37 -0800

    Update MultiStageGraph_challenge_6.js

--- Graph/MultiStageGraph_challenge_6.js
+++ Graph/MultiStageGraph_challenge_6.js
@@ -51,3 +51,9 @@ let graph = [//0  1  2  3  4  5  6  7  8
   stage = 4;
 let path = findShotestPath(graph, stage);
 console.log('path = ', path);
+
+
+// cost = [0,9,7,11,12,6,4,5,0]
+// direction = [undefined,2,6,6,5,8,8,8,undefined]
+// 'path = '
+// '1 ----> 2 ----> 6 ----> 8 ----> '
