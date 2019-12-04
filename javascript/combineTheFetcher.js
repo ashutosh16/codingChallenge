f8e642d5a665b885b639716ca5d0b576d4f4b522
Sameer Girkar <sam.girkar@gmail.com>
2019-12-03 17:51:19 -0800

    Create combineTheFetcher.js

---
+++ javascript/combineTheFetcher.js
@@ -0,0 +1,5 @@
+// Create a function that will take a input as fetcherList and 
+// return a function with signature as function (callback, prefix)
+// return function will be called with callback and prefix
+// fetcher function will be call all fetcher function with prefix value and combine and return the response of all fetcher 
+// with callback function 
