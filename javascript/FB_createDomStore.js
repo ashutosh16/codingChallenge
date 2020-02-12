e3bb094f8598d066814e1972e38b42ed40d359cf
Sameer Girkar <sam.girkar@gmail.com>
2020-02-12 01:50:52 -0800

    Create FB_createDomStore.js

---
+++ javascript/FB_createDomStore.js
@@ -0,0 +1,4 @@
+//Write a class called DOMStore that stores a Node and a value (reimplement Map). DOMStore contains the following functions:
+has(node) // returns boolean
+get(node) // returns node or undefined
+set(node, value) // "upsert", update or insert
