98a7b8d6b72bf96ec8a7f0a6fc0ca6161ed53534
Sameer Girkar <sam.girkar@gmail.com>
2020-02-17 05:13:01 -0800

    Update mapObject.js

--- javascript/mapObject.js
+++ javascript/mapObject.js
@@ -1,1 +1,12 @@
-// squareIt([1, 2, 3], x => x*x); // { 1: 1, 2: 4, 3: 9 }
+// MapToObject([1, 2, 3], x => x*x); // { 1: 1, 2: 4, 3: 9 }
+
+
+function MapToObject(arr, fun){
+  return arr.reduce((acc, current) => {
+    acc[current] = fun(current);
+    return acc;
+  }, {});
+}
+
+console.log(MapToObject([1, 2, 3], x => x*x));
+//{ 1: 1, 2: 4, 3: 9 }
