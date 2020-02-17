ddb9a6d5ec1d71226502dfa73cdc0ac090ae7f8b
Sameer Girkar <sam.girkar@gmail.com>
2020-02-17 01:43:36 -0800

    Update removeFalsyElementFromArray.js

--- javascript/removeFalsyElementFromArray.js
+++ javascript/removeFalsyElementFromArray.js
@@ -1,1 +1,16 @@
 //Removes elements from an array for which the given function returns false.
+var a = [0,1,2,3,4,5,6];
+
+function Remove(arr, callback){
+  
+  for(let i = 0; i <= arr.length-1; i++){
+    if(!callback(arr[i])) {
+      arr.splice(i,1);
+      i--;
+    }
+  }
+  return arr;
+}
+
+Remove(a, p => (p % 2 === 0));
+console.log(a);
