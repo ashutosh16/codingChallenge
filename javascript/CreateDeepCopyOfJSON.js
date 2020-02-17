e9e3962eb06d12fe97f7df2daf5c729169225d6c
Sameer Girkar <sam.girkar@gmail.com>
2020-02-17 02:44:17 -0800

    Update CreateDeepCopyOfJSON.js

--- javascript/CreateDeepCopyOfJSON.js
+++ javascript/CreateDeepCopyOfJSON.js
@@ -1,1 +1,31 @@
 
+function DeepCopy(obj, createKeyFun){
+  if(Array.isArray(obj)) {
+    return obj.map(item => DeepCopy(item, createKeyFun));
+  } else
+    if(obj !== null && typeof obj === 'object'){
+      return Object.keys(obj).reduce((acc, current) => {
+        let key = createKeyFun
+          ? createKeyFun(current)
+          : current;
+
+        acc[key] = DeepCopy(obj[current], createKeyFun);
+        return acc;
+      }, {});
+    } else {
+      return obj;
+    }
+}
+
+const obj = {
+  foo: '1',
+  nested: {
+    child: {
+      withArray: [
+        {
+          grandChild: ['hello']
+        }
+      ]
+    }
+  }
+};
