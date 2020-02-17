79cb5e2155fe690d7ab80be5406340bde403835f
Sameer Girkar <sam.girkar@gmail.com>
2020-02-17 03:45:33 -0800

    Update GroupTheGivenArray.js

--- javascript/GroupTheGivenArray.js
+++ javascript/GroupTheGivenArray.js
@@ -4,3 +4,18 @@
 // GroupWith([1, 2], [10, 20], [100, 200], (a, b, c) => a + b + c);
 // [111,222]
 
+function GroupWith(...args){
+  let groupfunction = args.pop();
+  let resultLength = args[0].length;
+  let result = [];
+  
+  for(let i=0; i <= resultLength-1; i++){
+    let argumentList = [];
+    args.forEach((arr => argumentList.push(arr[i])));
+    result.push(groupFunction(...argumentList));
+  }
+  return result;
+}
+
+GroupWith([1, 2], [10, 20], [100, 200], (a, b, c) => a + b + c);
+//[111, 222]
