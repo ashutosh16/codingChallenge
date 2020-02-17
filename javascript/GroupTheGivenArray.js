d57f935e70b059268ad6975e1e0c317149c57698
Sameer Girkar <sam.girkar@gmail.com>
2020-02-17 03:33:19 -0800

    Create GroupTheGivenArray.js

---
+++ javascript/GroupTheGivenArray.js
@@ -0,0 +1,6 @@
+// Creates an array of elements, grouped based on the position in the original arrays and 
+// using function as the last value to specify how grouped values should be combined.
+
+// GroupWith([1, 2], [10, 20], [100, 200], (a, b, c) => a + b + c);
+// [111,222]
+
