9983c6fc96f2ff0320d213c2ed4f264f944ff290
Sameer Girkar <sam.girkar@gmail.com>
2020-02-17 01:58:33 -0800

    Create chainPromise.js

---
+++ javascript/chainPromise.js
@@ -0,0 +1,10 @@
+// TODO
+
+// This is this how we can chain the promise.
+// .then(fun) -> Then function always return the resolved promise with return value of given function.
+// .catch(fun) -> Catch also always return the resolved promise with return value of given function.
+// Hence we can chain then with then also then with catch
+// eg.
+
+// abc(() => new Promise(res => setTimeout( () => resolve('result'), 2000)) ).then(x => x).then(x => x);
+// abc(5).thencatch()
