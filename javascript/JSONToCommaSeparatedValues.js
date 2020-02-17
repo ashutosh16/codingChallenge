3740c052316efddc26245a981f9a04496c176e39
Sameer Girkar <sam.girkar@gmail.com>
2020-02-17 04:56:26 -0800

    Update JSONToCommaSeparatedValues.js

--- javascript/JSONToCommaSeparatedValues.js
+++ javascript/JSONToCommaSeparatedValues.js
@@ -1,1 +1,9 @@
+// Example
+// JSONtoCSV([{ a: 1, b: 2 }, { a: 3, b: 4, c: 5 }, { a: 6 }, { b: 7 }], ['a', 'b'])
+// 'a,b "1","2" "3","4" "6","" "","7"'
 
+
+function JSONtoCSV(arr, column, seperator){
+  let result = '';
+  
+}
