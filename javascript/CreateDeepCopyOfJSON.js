d863683706fb460075f69aa5b66853a77e292d9b
Sameer Girkar <sam.girkar@gmail.com>
2020-02-17 02:57:54 -0800

    Update CreateDeepCopyOfJSON.js

--- javascript/CreateDeepCopyOfJSON.js
+++ javascript/CreateDeepCopyOfJSON.js
@@ -23,9 +23,16 @@ const obj = {
     child: {
       withArray: [
         {
-          grandChild: ['hello']
+          grandChild1: ['hello' , { grandGrandObj: 'hello grand grand obj1'}],
+          grandChild2: [{ grandGrandObj: 'hi grand grand obj2'}, { grandGrandObj: 'hello grand grand obj2'}]
         }
       ]
     }
   }
 };
+
+const upperKeysObj = DeepCopy(obj2, key => key.toUpperCase());
+
+//Output
+//{ FOO: '1', NESTED: { CHILD: { WITHARRAY: [{ GRANDCHILD1: ['hello', { GRANDGRANDOBJ: 'hello grand grand obj1' }], GRANDCHILD2: [{ GRANDGRANDOBJ: 'hi grand grand obj2' }, { GRANDGRANDOBJ: 'hello grand grand obj2' }] }] } } }
+
