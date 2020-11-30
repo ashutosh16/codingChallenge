a9db654828d41a22099b9fe36c20bd1ce5cf2fb7
Sameer Girkar <sam.girkar@gmail.com>
2020-11-29 16:02:20 -0800

    Update OwnFramworkLibrary.js

--- javascript/OwnFramworkLibrary.js
+++ javascript/OwnFramworkLibrary.js
@@ -2,12 +2,49 @@
     var Greeter = function (firstName, lastName, language){
         return new Greeter.init(firstName, lastName, language);
     }
-    Greeter.prototype = {};
+
+    var supportedLanguages = ['en', 'es'];
+    var greetings = {
+        'en' : 'Hello',
+        'es' : 'Hola'
+    };
+    var formalGreetings = {
+        'en' : 'Greetings',
+        'es' : 'Saludos'
+    }
+    Greeter.prototype = {
+        greet : function (isFormal){
+            console.log(isFormal ? this.greeting() : this.formalGreeting());
+            return this;
+        },
+        greeting: function (){
+            return `${greetings[this.language]} ${this.firstName}`;
+        },
+        formalGreeting: function (){
+            return `${formalGreetings[this.language]} ${this.getFullName()}`;
+        },
+        getFullName: function () {
+            return `${this.firstName} ${this.lastName}`;
+        },
+        setLanguage: function (lang) {
+            if(supportedLanguages[lang]){
+                this.language = lang;
+            } else {
+                throw 'Unsupported language';
+            }
+            return this;
+        }
+    };
     
     Greeter.init = function(firstName, lastName, language) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.language = language;
+        if(supportedLanguages.indexOf(language) > -1){
+             this.language = language;
+        } else {
+            this.language = 'en';
+            throw 'not supported language, defaulting to english';
+        }
     }
     Greeter.init.prototype = Greeter.prototype;
     global.Greeter = global.$G = Greeter;
