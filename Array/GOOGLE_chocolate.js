03bbe729216dc45d093561a09f8a1dff5d5345f3
Sameer Girkar <sam.girkar@gmail.com>
2020-12-12 15:29:18 -0800

    Update GOOGLE_chocolate.js

--- Array/GOOGLE_chocolate.js
+++ Array/GOOGLE_chocolate.js
@@ -2,3 +2,5 @@
 // Given a chocolate bar with n cuts with different sweetness.
 // we need to promise the min sweetness to each person.
 // 
+// Reference: https://leetcode.com/discuss/interview-question/350800/Google-or-Onsite-or-Chocolate-Sweetness
+// https://code.dennyzhang.com/divide-chocolate
