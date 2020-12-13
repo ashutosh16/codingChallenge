1ad690e1e9de0796423dcb04c757395918311823
Sameer Girkar <sam.girkar@gmail.com>
2020-12-13 02:03:16 -0800

    Update Array_FindEquilibriumIndexOfArray.js

--- Array/Array_FindEquilibriumIndexOfArray.js
+++ Array/Array_FindEquilibriumIndexOfArray.js
@@ -10,17 +10,17 @@
 {
   function findEquilibriumIndex(a){
     let leftSum = [0],
-      rightSum = Array(a.length),
+      rightSum = 0,
       equilibriumIndex = [];
     for(let i = 1; i < a.length; i++){
       leftSum[i] = leftSum[i-1] + a[i-1];
     }
-    rightSum[a.length -1] = 0;
-    (rightSum[a.length - 1] === leftSum[a.length - 1]) ? equilibriumIndex.push(a.length-1) : null;
+    
+    (leftSum[a.length - 1] === 0) ? equilibriumIndex.push(a.length-1) : null;
 
     for (let i = a.length -2; i >= 0; i--) {
-      rightSum[i] = rightSum[i + 1] + a[i + 1];
-      if(rightSum[i] === leftSum[i]){
+      rightSum = rightSum + a[i + 1];
+      if(rightSum === leftSum[i]){
         equilibriumIndex.push(i);
       }
     }
