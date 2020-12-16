0c709a856c56790c918a279b52c301e3778bc944
Sameer Girkar <sam.girkar@gmail.com>
2020-12-16 01:49:55 -0800

    Update LargestSumContiguousSubarray.js

--- Array/LargestSumContiguousSubarray.js
+++ Array/LargestSumContiguousSubarray.js
@@ -4,6 +4,37 @@
  * Time complexity O(n)
  */
 
+/**
+  * Keep on adding to the sum untill sum is greater than 0, 
+  * if sum is greater than 0 then 
+*/
+const LargestSumOfContinousSubArrays = (arr) => {
+  if (arr.length <= 1) return arr;
+  let currentSum = arr[0],
+    result = {
+      startIndex: 0,
+      lastIndex: 0,
+      sum: currentSum,
+    },
+    startIndex = 0,
+    lastIndex = 0;
+
+  for (let i = 1; i < arr.length; i++) {
+    currentSum += arr[i];
+    if (currentSum > result.sum) {
+      result.sum = currentSum;
+      result.lastIndex = i;
+      result.startIndex = startIndex;
+    }
+    // Important to keep this condition below (currentSum > result.sum) to manage the case of full -ve array.
+    if (currentSum <= 0) { 
+      startIndex = i + 1;
+      currentSum = 0;
+    }
+  }
+  return result;
+};
+
 //  Create 2 variabe maxSum and currentSum 
 // add element in currentSum as long as currentSum is +ve
 // if currentSum > maxSum then maxSum = currentSum
@@ -28,3 +59,5 @@ function LargesSumContiguousSubarray(a) {
 
 var LargestSum = LargesSumContiguousSubarray([-2, -3, 4, -1, -2, 1, 5, -3]);
 alert(" Largest sum "+ LargestSum); 
+
+
