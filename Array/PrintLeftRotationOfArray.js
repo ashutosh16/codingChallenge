399362d5143c38430badee55a3b6950c9783ae20
Sameer Girkar <sam.girkar@gmail.com>
2020-12-16 04:08:25 -0800

    Update PrintLeftRotationOfArray.js

--- Array/PrintLeftRotationOfArray.js
+++ Array/PrintLeftRotationOfArray.js
@@ -1,18 +1,17 @@
 // Print Left rotation of array at given point 
 function LeftRotation (arr, r) {
-	let count = 0;
+  let count = 0;
   let n = arr.length; 
-
-	for( let i = 0; i < n; i++) {
-		let j = (i - r + n) % n;
-		let temp = arr[i];
-		while(count < n) {
-            [arr[j] , temp] = [temp, arr[j]];
-            count ++;
-            if(count === n || i === j) break;
-            j = (j - r + n) % n;
-        }
-        if(count === n ) break;
+  for( let startIndex = 0; count < n; startIndex++) {
+    let nextIndex = (startIndex - r + n) % n;
+    let prevElement = arr[startIndex];
+    while(count < n) {
+      [arr[nextIndex] , prevElement] = [prevElement, arr[nextIndex]];
+      count ++;
+      if(count === n || startIndex === nextIndex) break;
+	nextIndex = (nextIndex - r + n) % n;
+      }
+      if(count === n ) break;
     }
     return arr;
 }
