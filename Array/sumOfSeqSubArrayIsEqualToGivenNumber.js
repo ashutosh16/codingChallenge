4fe46f03ba44dbaa99688ea117b0a741ed3c49e2
Sameer Girkar <sam.girkar@gmail.com>
2020-12-17 02:37:14 -0800

    Update sumOfSeqSubArrayIsEqualToGivenNumber.js

--- Array/sumOfSeqSubArrayIsEqualToGivenNumber.js
+++ Array/sumOfSeqSubArrayIsEqualToGivenNumber.js
@@ -28,3 +28,31 @@ function getSeq(a, resultSum) {
 }
 
 getSeq([2,3,4,5,7,2,3], 14);
+
+getSeq([-2, 5, 5, -4],4); // failed with not found
+
+{
+  function SubArrayWithGivenSum(a, n) {
+    let subArray=[],
+      sum=0,
+      map={};
+
+      for(let i=0; i < a.length; i++) {
+        sum+= a[i];
+        if(sum === n){
+          subArray.push(a.slice(0, i+1));
+        } else 
+          if (sum > n && typeof map[sum-n] !== 'undefined') {
+            subArray.push(a.slice(map[sum - n], i+1));
+          } else 
+            if (!map[sum]) {
+              map[sum] = i+1;
+            }
+      }
+      return subArray;
+  }
+
+  let result = SubArrayWithGivenSum([ -2, 5, 5, -4, 7, 6], 4);
+  console.log(result);
+  // [-2, 5, 5, -4]
+}
