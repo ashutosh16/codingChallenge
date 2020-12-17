b324ab06ec01fe08ac325c057143d56788eeb394
Sameer Girkar <sam.girkar@gmail.com>
2020-12-16 23:49:18 -0800

    Update findMaxSum.js

--- Array/findMaxSum.js
+++ Array/findMaxSum.js
@@ -1,7 +1,36 @@
 // Find maximum value of Sum( i*arr[i]) with only rotations on given array allowed
-//  for (int j=1; j<n; j++) 
-// {
-//   currVal = currVal + arrSum - n * arr[n - j];
-//   if (currVal > maxVal)
-//     maxVal = currVal;
-// }
+function maxSum(arr) {
+	let arraySum = 0;
+	let currentSum = 0;
+	let n = arr.length;
+  arr.forEach((item, index) => {
+    arraySum += item;
+    currentSum += index*item;
+  });
+  let maxSum = currentSum;
+  let leftRotation = 0;
+  for(let i=1; i < n; i++) {
+    currentSum = currentSum - arraySum + (arr[i-1] * n);
+    if(currentSum > maxSum) {
+      maxSum = currentSum;
+      leftRotation = i;
+    }
+  }
+  return {
+    maxSum,
+    leftRotation
+  }
+}
+
+// Hint  - Each time we do left rotation contribution of elements from index 1 to n-1 is decresed by 1 (arraySum - arr[i-1]). and contribution of arr[i-1] is incresed by n-1 times
+// next_sum = currentSum - (arraySum - arr[i-1]) + ((n-1) * arr[i-1])
+// next_sum = currentSum - arraySum + arr[i-1] + (n-1) * arr[i-1]
+// next_sum = currentSum - arraySum + (n * arr[i-1]) 
+
+//  0 1 2 3            <----------->
+// [8,3,1,2] -> 8*0 + 3*1 + 1*2 + 2*3 -> 
+// [3,1,2,8] -> 3*0 + 1*1 + 2*2 + 8*3 -> 
+// [1,2,8,3] -> 1*0 + 2*1 + 8*2 + 3*3 -> 
+// [2,8,3,1] -> 2*0 + 8*1 + 3*2 + 1*3 -> 
+
+// reference https://www.youtube.com/watch?v=3YNs_Ggqb-Q
