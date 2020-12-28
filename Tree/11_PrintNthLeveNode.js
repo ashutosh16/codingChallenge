25af7d2c1af4bf86921941446aeb39c606659039
Sameer Girkar <sam.girkar@gmail.com>
2020-12-27 21:44:19 -0800

    Update 11_PrintNthLeveNode.js

--- Tree/11_PrintNthLeveNode.js
+++ Tree/11_PrintNthLeveNode.js
@@ -5,9 +5,16 @@
     this.left = null;
     this.right = null;
   }
+  
+    let root = new Node(1);
+    root.left = new Node(2);
+    root.right = new Node(3);
+    root.left.left = new Node(4);
+    root.left.right = new Node(5);
+    root.right.left = new Node(6);
+    root.right.right = new Node(7);
 
   function printAllNodesAtGivenLevel(root, n, nodeList = '') {
-    debugger;
     if(root === null) {
       return nodeList;
     }
@@ -23,19 +30,21 @@
     }
     return nodeList;
   }
-
-
-  let root = new Node(1);
-  root.left = new Node(2);
-  root.right = new Node(3);
-  root.left.left = new Node(4);
-  root.left.right = new Node(5);
-  root.right.left = new Node(6);
-  root.right.right = new Node(7);
-
+  
+  
   let nodeList = printAllNodesAtGivenLevel(root, 3);
   console.log(nodeList);
+  
+  //Alternate solution
+  function FindNthLevelNodes(root, level, currentLevel =1 , resultList =[]) { debugger;
+    if(!root) return resultList;
+    if(currentLevel === level) return resultList.push(root.data) && resultList;
+    (root.left) && FindNthLevelNodes(root.left, level, currentLevel+1, resultList);
+    (root.right) && FindNthLevelNodes(root.right, level, currentLevel+1, resultList);
+    return resultList;
+ }
 
+  FindNthLevelNodes(root, 3);
 
   //Input
   /*         1        */
