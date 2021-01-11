da010c502afba2d380050d1add2109cc75c7069a
Sameer Girkar <sam.girkar@gmail.com>
2021-01-11 02:24:32 -0800

    Update Medium_Delete Nodes And Return Forest.js

--- leetCode/Medium_Delete Nodes And Return Forest.js
+++ leetCode/Medium_Delete Nodes And Return Forest.js
@@ -1,3 +1,4 @@
+// https://leetcode.com/problems/delete-nodes-and-return-forest/
 // Given the root of a binary tree, each node in the tree has a distinct value.
 // After deleting all nodes with a value in to_delete, we are left with a forest (a disjoint union of trees).
 // Return the roots of the trees in the remaining forest.  You may return the result in any order.
@@ -21,15 +22,6 @@
 // to_delete.length <= 1000
 // to_delete contains distinct values between 1 and 1000.
 
-
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
 
  //Definition for a binary tree node.
  function TreeNode(val, left, right) {
