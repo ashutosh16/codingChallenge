e45b00ab7d15814b3c325c36753bb18f638bdd64
Sameer Girkar <sam.girkar@gmail.com>
2021-01-24 15:55:36 -0800

    Dynamic programming with tabular method.
    Dynamic programming with tabular method.
    https://www.youtube.com/watch?v=obhWqDfzwQQ&list=PLEJXowNB4kPxBwaXtRO1qFLpCzF75DYrS&index=9

--- leetCode/Medium_Partition Equal Subset Sum.js
+++ leetCode/Medium_Partition Equal Subset Sum.js
@@ -1,6 +1,6 @@
 // Partition Equal Subset Sum
 // https://leetcode.com/problems/partition-equal-subset-sum/
-
+// https://www.youtube.com/watch?v=obhWqDfzwQQ&list=PLEJXowNB4kPxBwaXtRO1qFLpCzF75DYrS&index=9
 // Given a non-empty array nums containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
 
 // Example 1:
