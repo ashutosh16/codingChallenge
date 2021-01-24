ddf84b9fa49d199731b74bc7b38356873f8fa132
Sameer Girkar <sam.girkar@gmail.com>
2021-01-23 18:48:29 -0800

    Update 01 Knapsack using Recursion.js

--- DynamicProgramming/01 Knapsack using Recursion.js
+++ DynamicProgramming/01 Knapsack using Recursion.js
@@ -1,7 +1,7 @@
 
 //https://www.youtube.com/watch?v=mGfK-j9gAQA&list=PLEJXowNB4kPxBwaXtRO1qFLpCzF75DYrS&index=4
 
-function Knapsack(wt/*weightList*/, profit */profitList*/, w /*reamining bagWeight*/, n /*remainingItems*/) {
+function Knapsack(wt /*weightList*/, profit */profitList*/, w /*reamining bagWeight*/, n /*remainingItems*/) {
 
   if(n === 0 || w === 0) return 0;
   
@@ -11,6 +11,6 @@ function Knapsack(wt/*weightList*/, profit */profitList*/, w /*reamining bagWeig
   }
   
   // took the max profit if we skip the item or we include the item
-  return Math.max( Knapsack(wt, pl, w, n-1), profit[n] + Knapsack(wt, pl, w - wl[n], n-1) );
+  return Math.max( Knapsack(wt, profit, w, n-1), profit[n] + Knapsack(wt, profit, w - wl[n], n-1) );
 
 }
