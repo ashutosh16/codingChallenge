6c91823f8aeceb0c425d45bc3feb9e3a6355220a
Sameer Girkar <sam.girkar@gmail.com>
2021-01-24 12:47:47 -0800

    Update 3_sum of subsets backtracking.js

--- DynamicProgramming/3_sum of subsets backtracking.js
+++ DynamicProgramming/3_sum of subsets backtracking.js
@@ -1,2 +1,40 @@
 // backtracking sum of subsets
+// Find is subset of the arr is present in given arr with given sum.
+
+
+//Time complexity is O(2^n)
+
+function isSubsetPresent(arr, sum, n = arr.length-1) {
+  if(sum === 0) return true;
+  if(n === -1) return false;
+  
+  if(arr[n] > sum) return isSubsetPresent(arr, sum, n-1);
+  
+  return (
+    isSubsetPresent(arr, sum, n-1) ||
+    isSubsetPresent(arr, sum - arr[n], n-1)
+  );
+}
+
+isSubsetPresent([3, 34, 4, 12, 5, 2], 38);
+
+
+// With memorization 
+function isSubsetPresent(arr, sum, n = arr.length-1, map = {}) {
+  if(sum === 0) return true;
+  if(n === -1) return false;
+  if(typeof map[`${sum}-${n}`] !== 'undefined') return map[`${sum}-${n}`]; // return if result was already calculated
+  
+  let result = false;
+  
+  if(arr[n] > sum) result = isSubsetPresent(arr, sum, n-1, map);
+  
+  result = (
+    isSubsetPresent(arr, sum, n-1, map) ||
+    isSubsetPresent(arr, sum - arr[n], n-1, map)
+  );
+  
+  return map[`${sum}-${n}`] = result; // memorizing the calculated result.
+}
+isSubsetPresent([3, 34, 4, 12, 5, 2], 38);
 
