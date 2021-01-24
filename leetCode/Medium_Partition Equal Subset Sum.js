ab483c0f1705dda3d34d3fb073fc83e8d5b27a7d
Sameer Girkar <sam.girkar@gmail.com>
2021-01-24 15:54:29 -0800

    Update Medium_Partition Equal Subset Sum.js

--- leetCode/Medium_Partition Equal Subset Sum.js
+++ leetCode/Medium_Partition Equal Subset Sum.js
@@ -1,18 +1,20 @@
 // Partition Equal Subset Sum
 // https://leetcode.com/problems/partition-equal-subset-sum/
 
-Given a non-empty array nums containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
+// Given a non-empty array nums containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
 
-Example 1:
+// Example 1:
+// Input: nums = [1,5,11,5]
+// Output: true
+// Explanation: The array can be partitioned as [1, 5, 5] and [11].
 
-Input: nums = [1,5,11,5]
-Output: true
-Explanation: The array can be partitioned as [1, 5, 5] and [11].
-Example 2:
 
-Input: nums = [1,2,3,5]
-Output: false
-Explanation: The array cannot be partitioned into equal sum subsets.
+// Example 2:
+// Input: nums = [1,2,3,5]
+// Output: false
+// Explanation: The array cannot be partitioned into equal sum subsets.
+
+// Time complexity O(n * (sum/2))
 
 /**
  * @param {number[]} nums
@@ -52,3 +54,5 @@ var canPartition = function(nums) {
   }
   return m[row-1][col-1];
 };
+
+
