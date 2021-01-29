6e03337ad0000da9454ea97db1cc363906290ed9
Sameer Girkar <sam.girkar@gmail.com>
2021-01-29 02:47:06 -0800

    Update Medium_Find Leaves of Binary Tree.js

--- leetCode/Medium_Find Leaves of Binary Tree.js
+++ leetCode/Medium_Find Leaves of Binary Tree.js
@@ -1,3 +1,39 @@
+
+// Given a binary tree, collect a tree's nodes as if you were doing this: Collect and remove all leaves, repeat until the tree is empty.
+// https://leetcode.com/problems/find-leaves-of-binary-tree/
+// Example:
+
+// Input: [1,2,3,4,5]
+  
+//           1
+//          / \
+//         2   3
+//        / \     
+//       4   5    
+
+// Output: [[4,5,3],[2],[1]]
+ 
+
+// Explanation:
+
+// 1. Removing the leaves [4,5,3] would result in this tree:
+
+//           1
+//          / 
+//         2          
+ 
+
+// 2. Now removing the leaf [2] would result in this tree:
+
+//           1          
+ 
+
+// 3. Now removing the leaf [1] would result in the empty tree:
+
+//           []         
+// [[3,5,4],[2],[1]], [[3,4,5],[2],[1]], etc, are also consider correct answers since per each level it doesn't matter the order on which elements are returned.
+
+
 /**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
@@ -10,6 +46,7 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
+// Time Complexity O(n)
 var findLeaves = function(root) {
   if(!root)  return [];
   let result = [];
