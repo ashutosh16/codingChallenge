59692f238cbc31d54dc2e32d06bda474b0433089
Sameer Girkar <sam.girkar@gmail.com>
2021-02-06 23:38:35 -0800

    Update Hard_Maximal Rectangle.js

--- leetCode/Hard_Maximal Rectangle.js
+++ leetCode/Hard_Maximal Rectangle.js
@@ -1,4 +1,13 @@
-Maximal Rectangle
+// Maximal Rectangle
+// Given a rows x cols binary matrix filled with 0's and 1's, find the largest rectangle containing only 1's and return its area.
+
+// Example 1:
+
+// Input: matrix = [["1","0","1","0","0"],
+//                  ["1","0","1","1","1"],
+//                  ["1","1","1","1","1"],
+//                  ["1","0","0","1","0"]]
+// Output: 6 (from 1-4 to 2-4)
 
 /**
  * @param {character[][]} matrix
@@ -45,3 +54,5 @@ var maximalRectangle = function(matrix) {
   return resultArea;
     
 };
+
+
