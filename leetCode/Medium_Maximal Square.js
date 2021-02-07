7ae76a10140d734c4124c1b3789c3dc41750f5ac
Sameer Girkar <sam.girkar@gmail.com>
2021-02-06 19:32:50 -0800

    Update Medium_Maximal Square.js

--- leetCode/Medium_Maximal Square.js
+++ leetCode/Medium_Maximal Square.js
@@ -1,5 +1,6 @@
-Maximal Square
-Given an m x n binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
+// Maximal Square
+// Given an m x n binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
+// https://leetcode.com/problems/maximal-square/
 
 /**
  * @param {character[][]} matrix
