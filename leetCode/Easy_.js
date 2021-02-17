52260f92fc140abc3806009105d9b1933e9dc3d3
Sameer Girkar <sam.girkar@gmail.com>
2021-02-17 00:04:46 -0800

    Update Easy_.js

--- leetCode/Easy_.js
+++ leetCode/Easy_.js
@@ -1,29 +1,30 @@
-You are given a string s containing lowercase English letters, and a matrix shift, where shift[i] = [direction, amount]:
+// https://leetcode.com/problems/perform-string-shifts/
+// https://www.youtube.com/watch?v=sNH-f_5Gm0I&list=PLEJXowNB4kPwCPVjDv6KsAsThtDOCQUok&index=14
+// You are given a string s containing lowercase English letters, and a matrix shift, where shift[i] = [direction, amount]:
 
-direction can be 0 (for left shift) or 1 (for right shift). 
-amount is the amount by which string s is to be shifted.
-A left shift by 1 means remove the first character of s and append it to the end.
-Similarly, a right shift by 1 means remove the last character of s and add it to the beginning.
-Return the final string after all operations.
+// direction can be 0 (for left shift) or 1 (for right shift). 
+// amount is the amount by which string s is to be shifted.
+// A left shift by 1 means remove the first character of s and append it to the end.
+// Similarly, a right shift by 1 means remove the last character of s and add it to the beginning.
+// Return the final string after all operations.
 
- 
 
-Example 1:
+// Example 1:
+// Input: s = "abc", shift = [[0,1],[1,2]]
+// Output: "cab"
+// Explanation: 
+// [0,1] means shift to left by 1. "abc" -> "bca"
+// [1,2] means shift to right by 2. "bca" -> "cab"
 
-Input: s = "abc", shift = [[0,1],[1,2]]
-Output: "cab"
-Explanation: 
-[0,1] means shift to left by 1. "abc" -> "bca"
-[1,2] means shift to right by 2. "bca" -> "cab"
-Example 2:
 
-Input: s = "abcdefg", shift = [[1,1],[1,1],[0,2],[1,3]]
-Output: "efgabcd"
-Explanation:  
-[1,1] means shift to right by 1. "abcdefg" -> "gabcdef"
-[1,1] means shift to right by 1. "gabcdef" -> "fgabcde"
-[0,2] means shift to left by 2. "fgabcde" -> "abcdefg"
-[1,3] means shift to right by 3. "abcdefg" -> "efgabcd"
+// Example 2:
+// Input: s = "abcdefg", shift = [[1,1],[1,1],[0,2],[1,3]]
+// Output: "efgabcd"
+// Explanation:  
+// [1,1] means shift to right by 1. "abcdefg" -> "gabcdef"
+// [1,1] means shift to right by 1. "gabcdef" -> "fgabcde"
+// [0,2] means shift to left by 2. "fgabcde" -> "abcdefg"
+// [1,3] means shift to right by 3. "abcdefg" -> "efgabcd"
 
 /**
  * @param {string} s
@@ -40,7 +41,6 @@ var stringShift = function(s, shift) {
       r += shift[i][1];
     }
   }
-  console.log(l, r);
  
   let leftRotation =( (l > r) ? (l-r) : s.length - (r-l)) % s.length;
   
@@ -51,10 +51,7 @@ var stringShift = function(s, shift) {
     let prevElement = s[startIndex];
     do{
       nextIndex = (nextIndex - leftRotation + s.length) % s.length;
-      console.log(nextIndex);
-      
       [s[nextIndex], prevElement] = [prevElement, s[nextIndex]];
-      console.log(s);
       count++;
     } while(count < s.length && startIndex !== nextIndex)
   }
