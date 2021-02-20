e00dacf81213852ff24f20cb49be4c1d8c32fe8c
Sameer Girkar <sam.girkar@gmail.com>
2021-02-20 13:24:45 -0800

    Update sumOfSeqSubArrayIsEqualToGivenNumber.js

--- Array/sumOfSeqSubArrayIsEqualToGivenNumber.js
+++ Array/sumOfSeqSubArrayIsEqualToGivenNumber.js
@@ -3,60 +3,26 @@
   * Need to find the continous sub array whose total is equal to given result number.
   * Time complexity is O(n)
   */
-//This solution only work for the array with non-negative number.
-function getSeq(a, resultSum) {
-  var sum = 0,
-    i = 0,
-    j = 0;
-  sum = sum + a[j];
-
-  while(j < a.length && i < a.length) {
-    if(sum === resultSum){
-      //result array is from i to j
-      return a.slice(i,j+1);
-    } else 
-    if(sum < resultSum) {
-      j++;
-      sum = sum + a[j];
-    } else {
-      //sum > totalSum
-      sum = sum - a[i];
-      i++;
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraySum = function(nums, k) {
+  let map = {}; 
+  let sum = 0;
+  let count = 0;
+  for(let i=0; i<nums.length; i++){
+    sum += nums[i];  
+    if(sum === k) count++;
+    if(map[sum-k]){
+      count+= map[sum-k].length;
     }
+    map[sum] ? map[sum].push(i+1) : (map[sum]=[i+1]); // Make map of array because there can me multiplet sub array with same sume in that case we need to get both the valuse.
   }
-  return 'not found';
-
-}
-
-getSeq([2,3,4,5,7,2,3], 14);
-
-getSeq([-2, 5, 5, -4],4); // failed with not found
+  return count;
+};
 
 
-//Below solution work for the all +ve and -ve array.
-
-{
-  function SubArrayWithGivenSum(a, n) {
-    let subArray=[],
-      sum=0,
-      map={};
-
-      for(let i=0; i < a.length; i++) {
-        sum+= a[i];
-        if(sum === n){
-          subArray.push(a.slice(0, i+1));
-        } else 
-          if (sum > n && typeof map[sum-n] !== 'undefined') {
-            subArray.push(a.slice(map[sum - n], i+1));
-          } else 
-            if (!map[sum]) {
-              map[sum] = i+1;
-            }
-      }
-      return subArray;
-  }
-
-  let result = SubArrayWithGivenSum([ -2, 5, 5, -4, 7, 6], 4);
-  console.log(result);
-  // [-2, 5, 5, -4]
-}
+subarraySum([0,0,0,0], 0);
+//10
