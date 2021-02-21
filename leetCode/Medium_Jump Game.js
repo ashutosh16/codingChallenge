b05985c98a95a9d01f9fb17fb96bef5ffdd42c67
Sameer Girkar <sam.girkar@gmail.com>
2021-02-20 18:11:09 -0800

    Update Medium_Jump Game.js

--- leetCode/Medium_Jump Game.js
+++ leetCode/Medium_Jump Game.js
@@ -1,21 +1,20 @@
-Given an array of non-negative integers nums, you are initially positioned at the first index of the array.
+// https://leetcode.com/problems/jump-game/
+// https://www.youtube.com/watch?v=muDPTDrpS28&list=PLEJXowNB4kPwCPVjDv6KsAsThtDOCQUok&index=25
 
-Each element in the array represents your maximum jump length at that position.
+// Given an array of non-negative integers nums, you are initially positioned at the first index of the array.
+// Each element in the array represents your maximum jump length at that position.
+// Determine if you are able to reach the last index.
 
-Determine if you are able to reach the last index.
+// Example 1:
 
- 
+// Input: nums = [2,3,1,1,4]
+// Output: true
+// Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+// Example 2:
 
-Example 1:
-
-Input: nums = [2,3,1,1,4]
-Output: true
-Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
-Example 2:
-
-Input: nums = [3,2,1,0,4]
-Output: false
-Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+// Input: nums = [3,2,1,0,4]
+// Output: false
+// Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
 
 
 /**
@@ -34,3 +33,9 @@ var canJump = function(nums) {
   
   return false;
 };
+
+
+// Hint: Find the maximum index till you can jump (reachable)
+// If your i <= reachable means you can reach the i using some jump combinations.
+// If i is reachable then keep on cal culating the new reachable until reachable >= length-1
+// If reachable >= length-1 that means you can reach the end else you can not reach the end.
