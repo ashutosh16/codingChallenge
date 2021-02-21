a0ed613636baadb77fe722576248326fd50090e4
Sameer Girkar <sam.girkar@gmail.com>
2021-02-21 15:14:10 -0800

    Update Medium_Longest Common Subsequence.js

--- leetCode/Medium_Longest Common Subsequence.js
+++ leetCode/Medium_Longest Common Subsequence.js
@@ -1,25 +1,58 @@
-Given two strings text1 and text2, return the length of their longest common subsequence.
+// https://leetcode.com/problems/longest-common-subsequence/
+// https://www.youtube.com/watch?v=LAKWWDX3sGw&list=PLEJXowNB4kPwCPVjDv6KsAsThtDOCQUok&index=26
 
-A subsequence of a string is a new string generated from the original string with some characters(can be none) deleted without changing the relative order of the remaining characters. (eg, "ace" is a subsequence of "abcde" while "aec" is not). A common subsequence of two strings is a subsequence that is common to both strings.
+// Given two strings text1 and text2, return the length of their longest common subsequence.
+// A subsequence of a string is a new string generated from the original string with some characters(can be none) 
+// deleted without changing the relative order of the remaining characters. (eg, "ace" is a subsequence of "abcde" while "aec" is not). 
+// A common subsequence of two strings is a subsequence that is common to both strings.
 
- 
+// If there is no common subsequence, return 0.
 
-If there is no common subsequence, return 0.
+// Example 1:
 
- 
+// Input: text1 = "abcde", text2 = "ace" 
+// Output: 3  
+// Explanation: The longest common subsequence is "ace" and its length is 3.
+// Example 2:
 
-Example 1:
+// Input: text1 = "abc", text2 = "abc"
+// Output: 3
+// Explanation: The longest common subsequence is "abc" and its length is 3.
+// Example 3:
 
-Input: text1 = "abcde", text2 = "ace" 
-Output: 3  
-Explanation: The longest common subsequence is "ace" and its length is 3.
-Example 2:
+// Input: text1 = "abc", text2 = "def"
+// Output: 0
+// Explanation: There is no such common subsequence, so the result is 0.
 
-Input: text1 = "abc", text2 = "abc"
-Output: 3
-Explanation: The longest common subsequence is "abc" and its length is 3.
-Example 3:
 
-Input: text1 = "abc", text2 = "def"
-Output: 0
-Explanation: There is no such common subsequence, so the result is 0.
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+var longestCommonSubsequence = function(text1, text2) {
+  let row = text1.length+1;
+  let col = text2.length+1;
+  let table = Array(row);
+  
+  for(let i=0; i < row; i++){
+    table[i] = Array(col);
+    table[i][0] = 0;
+  }
+  
+  for(let j=1; j < col; j++){
+    table[0][j] = 0;
+  }
+  
+  for(let i=1; i<row; i++){
+    for(let j=1; j<col; j++){
+      table[i][j] = (text1[i-1] === text2[j-1]) 
+                    ? table[i-1][j-1] + 1
+                    : Math.max(table[i-1][j], table[i][j-1]);
+    }
+  }
+  
+  return table[row-1][col-1];
+};
+
+
