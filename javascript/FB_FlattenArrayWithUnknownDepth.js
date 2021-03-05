6b9b1200fe676a24eef5c985b5d3f8f5e17999c0
Sameer Girkar <sam.girkar@gmail.com>
2021-03-04 23:37:55 -0800

    Update FB_FlattenArrayWithUnknownDepth.js

--- javascript/FB_FlattenArrayWithUnknownDepth.js
+++ javascript/FB_FlattenArrayWithUnknownDepth.js
@@ -1,18 +1,16 @@
 //Flatten an array with unknown depth (reimplement .flat(Infinity)).
 //a. Write it both recursively and iteratively.
 
-function flat(a, result=[]){
-    if(typeof a === "object"){
-        for(var i=0; i<= a.length-1; i++){
-            flat(a[i], result);
-        }
-    } else {
-        result.push(a);
-    }
+function Flat(data, result=[]){
+    (Array.isArray(data))
+        ? data.map((item)=> Flat(item, result))
+        : result.push(data);
     return result;
+    
 }
 
 var arr = [1, 2, [3, 4, [5, 6]]];
 
 flat(arr);
 //[1, 2, 3, 4, 5, 6]
+
