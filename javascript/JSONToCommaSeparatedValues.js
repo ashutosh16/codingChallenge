c183a750fdebd5eaaa3dcddc5a9ed16af3862cb9
Sameer Girkar <sam.girkar@gmail.com>
2021-03-05 01:20:37 -0800

    Update JSONToCommaSeparatedValues.js

--- javascript/JSONToCommaSeparatedValues.js
+++ javascript/JSONToCommaSeparatedValues.js
@@ -2,8 +2,24 @@
 // JSONtoCSV([{ a: 1, b: 2 }, { a: 3, b: 4, c: 5 }, { a: 6 }, { b: 7 }], ['a', 'b'])
 // 'a,b "1","2" "3","4" "6","" "","7"'
 
+function JSONtoCSV(arr, column, seperator) {
+  let result = column.join(seperator) + " ";
 
-function JSONtoCSV(arr, column, seperator){
-  let result = '';
-  
+  return (
+    result +
+    arr
+      .map((obj) => {
+        let str = "";
+        column.forEach((key, index) => {
+          index !== 0 && (str += seperator);
+          str += obj[key] ? `"${obj[key]}"` : '""';
+        });
+        return str;
+      })
+      .join(" ")
+  );
 }
+
+JSONtoCSV([{ a: 1, b: 2 }, { a: 3, b: 4, c: 5 }, { a: 6 }, { b: 7 }], ['a', 'b']);
+// a,b "1","2" "3","4" "6","" "","7"
+
