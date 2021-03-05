e08eb1270db64ccf9753bdee6e7b84f5c8d5bfd3
Sameer Girkar <sam.girkar@gmail.com>
2021-03-05 00:09:00 -0800

    Update FB_createDomStore.js

--- javascript/FB_createDomStore.js
+++ javascript/FB_createDomStore.js
@@ -2,3 +2,7 @@
 has(node) // returns boolean
 get(node) // returns node or undefined
 set(node, value) // "upsert", update or insert
+
+
+// ES6 Maps can have objects as keys, so you can use the Node as the key.
+
