e94a35540896c290de1a5a19ba0a5ee77b280852
Sameer Girkar <sam.girkar@gmail.com>
2021-03-08 02:12:44 -0800

    Create Event Delegation.js

---
+++ javascript/Event Delegation.js
@@ -0,0 +1,2 @@
+// https://www.youtube.com/watch?v=3KJI1WZGDrg&list=PLlasXeu85E9eV5xUEgrWUB8NAUvNZGsK0&index=7
+// Insted of adding event to all the child add event to the common parent and use e.target.id to get targeted element.
