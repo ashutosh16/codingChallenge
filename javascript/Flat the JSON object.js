cfae90e1e2b037c71249869977f2a88bd87fbf53
Sameer Girkar <sam.girkar@gmail.com>
2021-03-08 02:38:51 -0800

    Create Flat the JSON object.js

---
+++ javascript/Flat the JSON object.js
@@ -0,0 +1,35 @@
+{
+  const obj = {
+    name: "sameer",
+    office: {
+      name: "Infa",
+      address: {
+        street: "redwood",
+        pincode: "1234"
+      } 
+    }
+  
+  }
+  
+  
+  
+  function Flatern(obj, prefix="", result = {}) {
+    var keys = Object.keys(obj);
+    result = keys.reduce((acc, key) => {
+      let newName = prefix + key;
+      if(typeof obj[key] === "object" && Object.keys(obj[key]).length > 0) {
+        Flatern(obj[key], newName+"_", acc);
+      } else {
+        acc[newName] = obj[key];
+      }
+      return acc;
+    }, result);
+    return result;
+  }
+  
+  
+  const result = Flatern(obj);
+  console.log(result);
+  
+//   {"name":"sameer","office_name":"Infa","office_address_street":"redwood","office_address_pincode":"1234"}
+}
