7378e64bf1a25036899f76c2f4702e20684299c5
Sameer Girkar <sam.girkar@gmail.com>
2021-03-11 03:25:40 -0800

    Update findOutput.js

--- javascript/findOutput.js
+++ javascript/findOutput.js
@@ -37,6 +37,36 @@ obj.method(fn, 1);
 
 // ------------------------------------------------------------------------------------------------------------
 
+(function () {
+    try {
+        throw new Error();
+    } catch (x) {
+        var x = 1, y = 2;
+        console.log(x);
+    }
+    console.log(x);
+    console.log(y);
+})();
+
+// 1
+// undefined
+// 2
+
+(function () {
+    var x, y; // outer and hoisted
+    try {
+        throw new Error();
+    } catch (x /* inner */) {
+        x = 1; // inner x, not the outer one
+        y = 2; // there is only one y, which is in the outer scope
+        console.log(x /* inner */);
+    }
+    console.log(x);
+    console.log(y);
+})();
+
+// ------------------------------------------------------------------------------------------------------------
+
 function b() {
   console.log(value);
 }
