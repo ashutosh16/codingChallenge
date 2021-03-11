616f0f5995b174a45bfb378e526055ee889111c7
Sameer Girkar <sam.girkar@gmail.com>
2021-03-11 03:32:45 -0800

    Update findOutput.js

--- javascript/findOutput.js
+++ javascript/findOutput.js
@@ -88,6 +88,14 @@ a();
 */
 
 // ------------------------------------------------------------------------------------------------------------
+
+typeof undefined == typeof NULL
+
+// The expression will be evaluated to true, since NULL will be treated as any other undefined variable.
+// Note: JavaScript is case-sensitive and here we are using NULL instead of null.
+
+// ------------------------------------------------------------------------------------------------------------
+
 {
   let num1 = 12;
   let num2 = new Number(12);
