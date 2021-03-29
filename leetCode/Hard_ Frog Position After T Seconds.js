0d72d5d518a266c6f834c3ee0a68ba46c1da68bf
Sameer Girkar <sam.girkar@gmail.com>
2021-03-29 01:14:04 -0700

    Update Hard_ Frog Position After T Seconds.js

--- leetCode/Hard_ Frog Position After T Seconds.js
+++ leetCode/Hard_ Frog Position After T Seconds.js
@@ -1,5 +1,7 @@
 // https://leetcode.com/problems/frog-position-after-t-seconds/
-// Given an undirected tree consisting of n vertices numbered from 1 to n. A frog starts jumping from vertex 1. In one second, the frog jumps from its current vertex to another unvisited vertex if they are directly connected. The frog can not jump back to a visited vertex. In case the frog can jump to several vertices, it jumps randomly to one of them with the same probability. 
+// Given an undirected tree consisting of n vertices numbered from 1 to n. A frog starts jumping from vertex 1. 
+// In one second, the frog jumps from its current vertex to another unvisited vertex if they are directly connected. 
+// The frog can not jump back to a visited vertex. In case the frog can jump to several vertices, it jumps randomly to one of them with the same probability. 
 // Otherwise, when the frog can not jump to any unvisited vertex, it jumps forever on the same vertex.
 // The edges of the undirected tree are given in the array edges, where edges[i] = [ai, bi] means that exists an edge connecting the vertices ai and bi.
 // Return the probability that after t seconds the frog is on the vertex target.
@@ -7,7 +9,9 @@
 // Example 1:
 // Input: n = 7, edges = [[1,2],[1,3],[1,7],[2,4],[2,6],[3,5]], t = 2, target = 4
 // Output: 0.16666666666666666 
-// Explanation: The figure above shows the given graph. The frog starts at vertex 1, jumping with 1/3 probability to the vertex 2 after second 1 and then jumping with 1/2 probability to vertex 4 after second 2. Thus the probability for the frog is on the vertex 4 after 2 seconds is 1/3 * 1/2 = 1/6 = 0.16666666666666666. 
+// Explanation: The figure above shows the given graph. The frog starts at vertex 1,
+// jumping with 1/3 probability to the vertex 2 after second 1 and then jumping with 1/2 probability to vertex 4 after second 2.
+// Thus the probability for the frog is on the vertex 4 after 2 seconds is 1/3 * 1/2 = 1/6 = 0.16666666666666666. 
 
 
 // Example 2:
@@ -43,10 +47,14 @@ var frogPosition = function(n, edges, t, target) {
     }
     graph[edges[i][0]].push(edges[i][1]);
   }
+//   edges = [[1,2],[1,3],[1,7],[2,4],[2,6],[3,5]]
+  // graph = { '1': [ 2, 3, 7 ], '2': [ 4, 6 ], '3': [ 5 ] }
     
   return findTarget(1 /*currentNode**/, t, target, graph);
 };
 
+// Leaf node will aways return 1 or 0
+// frog will always stop at leaf node.
 function findTarget(currentNode, time, target, graph) {
   if(currentNode === target) {
     // If time is 0 means frog can not go further or if no child of current node then also frog will jump at target point
@@ -55,18 +63,30 @@ function findTarget(currentNode, time, target, graph) {
   }
   
   let child = graph[currentNode];
-  if(!child) return  0;
+  if(!child) return  0; // If no children then frog is block here. Frog will jump endlessly at same position.
   time --;
   if(time >= 0 ){
-    return Math.max(...child.graph(
+    return Math.max(...child.map(
       n => findTarget(n, time, target, graph)
     )) / child.length; // Divide by child length to get probability
   }
   return 0;
 }
 
+frogPosition(7, [[1,2],[1,3],[1,7],[2,4],[2,6],[3,5]], 2, 4);
+// 0.16667
 
 // frogPosition(7, [[1,2],[1,3],[1,7],[2,4],[2,6],[3,5]] , 2, 4);
+// 1 
+// |---------> 2
+//             |--------->4
+//             |--------->6
+// 
+// |---------> 3
+//             |--------->5
+// 
+// |---------> 7
+
 
 // frogPosition(7, [[1,2],[1,3],[1,7],[2,4],[2,6],[3,5]],20, 6);
 // 0.16667
