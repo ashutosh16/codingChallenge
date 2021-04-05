2cc50a57d64f98cb9847d93e2fc32c5ef197b5c4
Sameer Girkar <sam.girkar@gmail.com>
2021-04-05 00:18:26 -0700

    Update Array_reduce.js

--- javascript/Array_reduce.js
+++ javascript/Array_reduce.js
@@ -1,5 +1,29 @@
 //Implement The Array reduced method from JS.
 
+Array.prototype.myReduce = function(callback, acc) {
+    let count = 0;
+    if(!acc) {
+        acc = this[0];
+        count++;
+    }
+    let current = this[count];
+    count++;
+    while(count < this.length) {
+        acc = callback(acc, this[count]);
+        count++;
+    }
+    return acc;
+}
+
+
+const result = ["00", "01", "10", "11"].myReduce((acc, item)=>{ 
+    acc.push(`0${item}`); acc.push(`1${item}`);
+    return acc;
+}, []);
+// ["001", "101", "010", "110", "011", "111"]
+
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------
 var numbers = [175, 50, 25];
 
 function myFunc(total, num) {
