1188a1a5e3e55b1f019e1380410d8038a8b804ed
Sameer Girkar <sam.girkar@gmail.com>
2021-04-05 03:36:29 -0700

    Update FB_createDomStore.js

--- javascript/FB_createDomStore.js
+++ javascript/FB_createDomStore.js
@@ -1,8 +1,27 @@
 //Write a class called DOMStore that stores a Node and a value (reimplement Map). DOMStore contains the following functions:
-has(node) // returns boolean
-get(node) // returns node or undefined
-set(node, value) // "upsert", update or insert
-
+// has(node) // returns boolean
+// get(node) // returns node or undefined
+// set(node, value) // "upsert", update or insert
 
 // ES6 Maps can have objects as keys, so you can use the Node as the key.
+function DOMStore(){
+  const map = new Map();
+  return {
+    set: map.set.bind(map),
+    has: map.has.bind(map),
+    get: map.get.bind(map)
+  };
+}
+
+const map = DOMStore();
+map.set(document.getElementsByClassName("list-body")[2], "list-body 2");
+map.set(document.getElementsByClassName("list-body")[1], "list-body 1");
+
+map.has(document.getElementsByClassName("list-body")[2]); // true
+map.has(document.getElementsByClassName("list-body")[0]); // false
+
+map.get(document.getElementsByClassName("list-body")[1]); // "list-body 1"
+
+
+
 
