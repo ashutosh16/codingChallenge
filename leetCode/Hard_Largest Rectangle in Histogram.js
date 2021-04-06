3dba326efad938be140ed6ffe3e202e0b2a8c700
Sameer Girkar <sam.girkar@gmail.com>
2021-04-06 02:24:28 -0700

    Update Hard_Largest Rectangle in Histogram.js

--- leetCode/Hard_Largest Rectangle in Histogram.js
+++ leetCode/Hard_Largest Rectangle in Histogram.js
@@ -28,10 +28,10 @@
  */
 var largestRectangleArea = function(heights) {
   let n = heights.length;
-  let lR = Array(n);
-  let rR = Array(n);
-  let st = [];
-  let stR = [];
+  let lR = Array(n); // store the left most included index for each bar.
+  let rR = Array(n); // store the right most included index for each bar.
+  let st = []; // stack to find lR
+  let stR = []; // stack to find rR
 
   for(let i=0; i < n; i++){
     while(st.length && heights[st[st.length-1]] >= heights[i]) st.pop();
@@ -54,6 +54,7 @@ var largestRectangleArea = function(heights) {
   
   let result = 0;
   console.log(lR, rR);
+  // Calculate the rectangle area with each bar included fully.
   for(let i=0; i < n; i++){
     result = Math.max(result, (rR[i] - lR[i] + 1) * heights[i]);
   }
@@ -64,7 +65,11 @@ var largestRectangleArea = function(heights) {
 // Hint
 // // https://www.youtube.com/watch?v=vcv3REtIvEo&t=0s
 // When we try to calculate the max area. Atlease one bar is fully included in the result area.
-// Hence we calculate the area will be covered when 0 to n bar is filly included at a time.
+// Hence we calculate the area will be covered when 0 to n bar is fully included at a time.
 // example. When 0 bar is fully included,  1 bar is fully included,  2 bar is fully included etc 
-// to get the are we need to find what is left index and right index can be included in the current bar.
+// to get the area, we need to find what is left index and right index can be included in the current bar.
 // then to find area rightIndex-LeftInmdex+1 * hight of the current bar.
+
+// To get left index for all bar travel bar from left to rihgt with maintaining stack.
+// To get rihgt index for all bar travel bar from right to left with maintaining stack.
+
