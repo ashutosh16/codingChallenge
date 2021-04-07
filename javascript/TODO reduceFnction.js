06cd0df19566a4278982696337abc650946a434c
Sameer Girkar <sam.girkar@gmail.com>
2021-04-07 03:01:33 -0700

    Update TODO reduceFnction.js

--- javascript/TODO reduceFnction.js
+++ javascript/TODO reduceFnction.js
@@ -1,1 +1,20 @@
 // wrote reduce function
+
+Array.prototype.myReducer = function(callback, init) {
+  if(this.length === 0) return init;
+  let acc = null;
+  let index = 0;
+  if(typeof init !== "undefined") {
+    acc = init;
+  } else {
+    acc = this[index];
+    index++;
+  }
+  for(; index < this.length; index++) {
+    acc = callback(acc, this[index]);
+  }
+  return acc;
+}
+
+[1,2,3].myReducer((acc, item)=>acc+item,10);
+// 16
