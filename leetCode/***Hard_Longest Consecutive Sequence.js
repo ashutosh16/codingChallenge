11201a7c5e61b946eadc0474adfbb8abc2664a33
Sameer Girkar <sam.girkar@gmail.com>
2021-04-11 17:30:03 -0700

    Create ***Hard_Longest Consecutive Sequence.js

---
+++ leetCode/***Hard_Longest Consecutive Sequence.js
@@ -0,0 +1,31 @@
+// Longest Consecutive Sequence
+// Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+// Example 1:
+// Input: nums = [100,4,200,1,3,2]
+// Output: 4
+// Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+
+// Example 2:
+// Input: nums = [0,3,7,2,5,8,4,6,0,1]
+// Output: 9
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+  const s = new Set(nums);
+  let resultLen = 0;
+  for(let num of nums) {
+    if(s.has(num-1)) continue;
+    let len=1;
+    while(s.has(num+len)){
+      len++;
+    }
+    resultLen = Math.max(len, resultLen);
+  }
+  return resultLen;
+};
+
+longestConsecutive([100,4,200,1,3,2]);
+// 4
