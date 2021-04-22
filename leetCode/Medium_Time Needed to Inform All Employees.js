88a4052c0cce392179e87d0a57c51d770857f40d
Sameer Girkar <sam.girkar@gmail.com>
2021-04-22 02:43:33 -0700

    Update Medium_Time Needed to Inform All Employees.js

--- leetCode/Medium_Time Needed to Inform All Employees.js
+++ leetCode/Medium_Time Needed to Inform All Employees.js
@@ -70,6 +70,7 @@ var numOfMinutes = function(n, headID, manager, informTime) {
 
 numOfMinutes(7, 6, [1,2,3,4,5,6,-1], [0,6,5,4,3,2,1]);
 // 21
+//--------------------------------------------------------------------------------------------------------------------------------------------------
 
 //  TOP to Bottom BFS
 // Create graph with manager : [subordinate arr]
