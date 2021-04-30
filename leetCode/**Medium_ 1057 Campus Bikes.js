ce3a6c4cef48db1dafd94fd701f4ffce1c3c35c5
Sameer Girkar <sam.girkar@gmail.com>
2021-04-30 03:55:43 -0700

    Update and rename Medium_Campus Bikes.js to **Medium_ 1057 Campus Bikes.js

--- leetCode/**Medium_ 1057 Campus Bikes.js
+++ leetCode/**Medium_ 1057 Campus Bikes.js
@@ -1,5 +1,6 @@
-// On a campus represented as a 2D grid, there are N workers and M bikes, with N <= M. Each worker and bike is a 2D coordinate on this grid.
+// https://leetcode.com/problems/campus-bikes
 
+// On a campus represented as a 2D grid, there are N workers and M bikes, with N <= M. Each worker and bike is a 2D coordinate on this grid.\
 // Our goal is to assign a bike to each worker. Among the available bikes and workers, 
 // we choose the (worker, bike) pair with the shortest Manhattan distance between each other, and assign the bike to that worker. 
 // (If there are multiple (worker, bike) pairs with the same shortest Manhattan distance, we choose the pair with the smallest worker index; 
