a6f3a29bcd5a698000200ccc489cebdde82a2604
Sameer Girkar <sam.girkar@gmail.com>
2021-05-02 01:25:01 -0700

    Update Medium_ 1679 Max Number of K-Sum Pairs.js

--- leetCode/Medium_ 1679 Max Number of K-Sum Pairs.js
+++ leetCode/Medium_ 1679 Max Number of K-Sum Pairs.js
@@ -1,4 +1,6 @@
 // https://leetcode.com/problems/max-number-of-k-sum-pairs/
+// Find max number of pairs whose sum equals to given target.
+
 // You are given an integer array nums and an integer k.
 // In one operation, you can pick two numbers from the array whose sum equals k and remove them from the array.
 // Return the maximum number of operations you can perform on the array.
