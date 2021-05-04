123bc01ec82c7c32920c7c75305ff4ec95b4a4c6
Sameer Girkar <sam.girkar@gmail.com>
2021-05-04 00:37:10 -0700

    Update **Easy_binary Search to find less than or equal value.js

--- leetCode/**Easy_binary Search to find less than or equal value.js
+++ leetCode/**Easy_binary Search to find less than or equal value.js
@@ -1,19 +1,19 @@
 // This is not a question but used in multiple questions.
-
+// Binary search with recurssion
 var a = [24, 29, 31, 76, 81];
-  function find(t,l = 0, r =4){ 
-    const mid = Math.floor((l+r) / 2);
-    if(a[mid] === t) return t;
-    if(mid > 0 && t>=a[mid-1] && t < a[mid]) return a[mid-1];
-    
-    if(t < a[mid]) return find(t, l, mid-1);
-    return find(t, mid+1, r);
-  }
-  var input = [[28],[24],[29],[77],[30],[25],[76],[75],[81],[80]];
-  
-  input.forEach((t)=>{
-    console.log(t[0], find(t[0]));
-  });
+function find(t,l = 0, r =4){ 
+  const mid = Math.floor((l+r) / 2);
+  if(a[mid] === t) return t;
+  if(mid > 0 && t>=a[mid-1] && t < a[mid]) return a[mid-1];
+
+  if(t < a[mid]) return find(t, l, mid-1);
+  return find(t, mid+1, r);
+}
+var input = [[28],[24],[29],[77],[30],[25],[76],[75],[81],[80]];
+
+input.forEach((t)=>{
+  console.log(t[0], find(t[0]));
+});
   // 28 24
   // 24 24
   // 29 29
